fcc711bd877317bca8f2bd7aae64f06a3ca426da
fcharest <frederik.charest@radio-canada.ca>
2015-04-29 11:29:19 -0400

    licensing

--- src/url-utilities.js
+++ src/url-utilities.js
@@ -1,3 +1,6 @@
+// Copyright (c) CBC/Radio-Canada. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 define(['configs', 'lodash', 'router', 'array-utilities', 'string-utilities'],
     function(configs, _, router, arrayUtilities, stringUtitilies) {
         'use strict';
