aebca62d64e3577a7633463d7b11146220d01128
Maxime Forget-Séguin <maxime.forget-seguin@radio-canada.ca>
2016-05-24 13:24:05 -0400

    update to koco2

--- src/url-utilities.js
+++ src/url-utilities.js
@@ -1,94 +1,96 @@
 // Copyright (c) CBC/Radio-Canada. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-define(['configs', 'router', 'array-utilities', 'string-utilities', 'query'],
-    function(configs, router, arrayUtilities, stringUtitilies, Query) {
-        'use strict';
+import configs from 'configs';
+import router from 'router';
+import arrayUtilities from 'array-utilities';
+import stringUtitilies from 'string-utilities';
+import Query from 'query';
 
-        var UrlUtilities = function UrlUtilities() {};
 
-        UrlUtilities.prototype.urlBase64Decode = function(str) {
-            var output = str.replace('-', '+').replace('_', '/');
+var UrlUtilities = function UrlUtilities() {};
 
-            switch (output.length % 4) {
-                case 0:
-                    break;
-                case 2:
-                    output += '==';
-                    break;
-                case 3:
-                    output += '=';
-                    break;
-                default:
-                    throw 'Illegal base64url string!';
-            }
+UrlUtilities.prototype.urlBase64Decode = function(str) {
+    var output = str.replace('-', '+').replace('_', '/');
 
-            return window.atob(output); //TODO: polifyll https://github.com/davidchambers/Base64.js
-        };
+    switch (output.length % 4) {
+        case 0:
+            break;
+        case 2:
+            output += '==';
+            break;
+        case 3:
+            output += '=';
+            break;
+        default:
+            throw 'Illegal base64url string!';
+    }
 
-        UrlUtilities.prototype.isSameUrl = function(url1, url2) {
-            return cleanUrl(url1) === cleanUrl(url2);
-        };
+    return window.atob(output); //TODO: polifyll https://github.com/davidchambers/Base64.js
+};
 
-        UrlUtilities.prototype.url = function(url, baseUrl) {
-            baseUrl = (baseUrl || configs.baseUrl).replace(/^\/|\/$/g, '');
-            url = url.replace(/^\//g, '');
+UrlUtilities.prototype.isSameUrl = function(url1, url2) {
+    return cleanUrl(url1) === cleanUrl(url2);
+};
 
-            if(baseUrl){
-                return '/' + baseUrl + '/' + url;
-            }
+UrlUtilities.prototype.url = function(url, baseUrl) {
+    baseUrl = (baseUrl || configs.baseUrl).replace(/^\/|\/$/g, '');
+    url = url.replace(/^\//g, '');
 
-            return '/' + url;
-        };
+    if (baseUrl) {
+        return '/' + baseUrl + '/' + url;
+    }
 
-        UrlUtilities.prototype.patternWithQueryString = function(url) {
-            return this.url(url) + ':?queryString:';
-        };
+    return '/' + url;
+};
 
-        UrlUtilities.prototype.getReturnUrl = function(_default) {
-            var self = this;
-            var result = self.url(_default);
+UrlUtilities.prototype.patternWithQueryString = function(url) {
+    return this.url(url) + ':?queryString:';
+};
 
-            var context = router.viewModel();
+UrlUtilities.prototype.getReturnUrl = function(_default) {
+    var self = this;
+    var result = self.url(_default);
 
-            if (context && context.route) {
-                var query = new Query(context.route.url);
+    var context = router.viewModel();
 
-                if (query.params.returnTo) {
-                    result = query.params.returnTo;
-                }
-            }
+    if (context && context.route) {
+        var query = new Query(context.route.url);
 
-            return result || '';
-        };
-
-        UrlUtilities.prototype.joinUrlParts = function() {
-            var result = '';
-            var urlParts = Array.prototype.slice.call(arguments, 0);
-
-            if (arrayUtilities.isNotEmptyArray(urlParts)) {
-                for (var i = 0; i < urlParts.length; i++) {
-                    var urlPart = urlParts[i];
-                    var isLast = i == (urlParts.length - 1);
-
-                    if (urlPart && !isLast) {
-                        urlPart = stringUtitilies.trimRight(urlPart, '/');
-                        urlPart += '/';
-                    }
-
-                    result += urlPart;
-                }
-            }
-
-            return result;
-        };
-
-        function cleanUrl(url) {
-            var result = url || '';
-            result = result.replace(/#.*$/, '').replace(/^\/|\/$/g, '').toLowerCase();
-
-            return result;
+        if (query.params.returnTo) {
+            result = query.params.returnTo;
         }
+    }
 
-        return new UrlUtilities();
-    });
+    return result || '';
+};
+
+UrlUtilities.prototype.joinUrlParts = function() {
+    var result = '';
+    var urlParts = Array.prototype.slice.call(arguments, 0);
+
+    if (arrayUtilities.isNotEmptyArray(urlParts)) {
+        for (var i = 0; i < urlParts.length; i++) {
+            var urlPart = urlParts[i];
+            var isLast = i == (urlParts.length - 1);
+
+            if (urlPart && !isLast) {
+                urlPart = stringUtitilies.trimRight(urlPart, '/');
+                urlPart += '/';
+            }
+
+            result += urlPart;
+        }
+    }
+
+    return result;
+};
+
+function cleanUrl(url) {
+    var result = url || '';
+    result = result.replace(/#.*$/, '').replace(/^\/|\/$/g, '').toLowerCase();
+
+    return result;
+}
+
+export default new UrlUtilities();
