95534cc316b49f6bfe7756d2af2dd9452fb5791f
Maxime Séguin <info@maximeseguin.com>
2016-06-05 13:27:42 -0400

    koco2... from jquery promises to es2015 promises

--- dist/url-utilities.js
+++ dist/url-utilities.js
@@ -1,123 +1,135 @@
 (function (global, factory) {
-    if (typeof define === "function" && define.amd) {
-        define(['exports', 'koco-configs', 'koco-array-utilities', 'koco-string-utilities', 'koco-query'], factory);
-    } else if (typeof exports !== "undefined") {
-        factory(exports, require('koco-configs'), require('koco-array-utilities'), require('koco-string-utilities'), require('koco-query'));
-    } else {
-        var mod = {
-            exports: {}
-        };
-        factory(mod.exports, global.kocoConfigs, global.kocoArrayUtilities, global.kocoStringUtilities, global.kocoQuery);
-        global.urlUtilities = mod.exports;
-    }
-})(this, function (exports, _kocoConfigs, _kocoArrayUtilities, _kocoStringUtilities, _kocoQuery) {
-    'use strict';
-
-    Object.defineProperty(exports, "__esModule", {
-        value: true
-    });
-
-    var _kocoConfigs2 = _interopRequireDefault(_kocoConfigs);
-
-    var _kocoArrayUtilities2 = _interopRequireDefault(_kocoArrayUtilities);
-
-    var _kocoStringUtilities2 = _interopRequireDefault(_kocoStringUtilities);
-
-    var _kocoQuery2 = _interopRequireDefault(_kocoQuery);
-
-    function _interopRequireDefault(obj) {
-        return obj && obj.__esModule ? obj : {
-            default: obj
-        };
-    }
-
-    // Copyright (c) CBC/Radio-Canada. All rights reserved.
-    // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-    var UrlUtilities = function UrlUtilities() {};
-
-    UrlUtilities.prototype.urlBase64Decode = function (str) {
-        var output = str.replace('-', '+').replace('_', '/');
-
-        switch (output.length % 4) {
-            case 0:
-                break;
-            case 2:
-                output += '==';
-                break;
-            case 3:
-                output += '=';
-                break;
-            default:
-                throw 'Illegal base64url string!';
-        }
-
-        return window.atob(output); //TODO: polifyll https://github.com/davidchambers/Base64.js
+  if (typeof define === "function" && define.amd) {
+    define(['exports', 'koco-configs', 'koco-array-utilities', 'koco-string-utilities', 'koco-query', 'jquery', 'koco'], factory);
+  } else if (typeof exports !== "undefined") {
+    factory(exports, require('koco-configs'), require('koco-array-utilities'), require('koco-string-utilities'), require('koco-query'), require('jquery'), require('koco'));
+  } else {
+    var mod = {
+      exports: {}
     };
+    factory(mod.exports, global.kocoConfigs, global.kocoArrayUtilities, global.kocoStringUtilities, global.kocoQuery, global.jquery, global.koco);
+    global.urlUtilities = mod.exports;
+  }
+})(this, function (exports, _kocoConfigs, _kocoArrayUtilities, _kocoStringUtilities, _kocoQuery, _jquery, _koco) {
+  'use strict';
 
-    UrlUtilities.prototype.isSameUrl = function (url1, url2) {
-        return cleanUrl(url1) === cleanUrl(url2);
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+
+  var _kocoConfigs2 = _interopRequireDefault(_kocoConfigs);
+
+  var _kocoArrayUtilities2 = _interopRequireDefault(_kocoArrayUtilities);
+
+  var _kocoStringUtilities2 = _interopRequireDefault(_kocoStringUtilities);
+
+  var _kocoQuery2 = _interopRequireDefault(_kocoQuery);
+
+  var _jquery2 = _interopRequireDefault(_jquery);
+
+  var _koco2 = _interopRequireDefault(_koco);
+
+  function _interopRequireDefault(obj) {
+    return obj && obj.__esModule ? obj : {
+      default: obj
     };
+  }
 
-    UrlUtilities.prototype.url = function (url, baseUrl) {
-        baseUrl = (baseUrl || _kocoConfigs2.default.baseUrl).replace(/^\/|\/$/g, '');
-        url = url.replace(/^\//g, '');
+  // Copyright (c) CBC/Radio-Canada. All rights reserved.
+  // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-        if (baseUrl) {
-            return '/' + baseUrl + '/' + url;
-        }
+  var UrlUtilities = function UrlUtilities() {};
 
-        return '/' + url;
-    };
+  UrlUtilities.prototype.urlBase64Decode = function (str) {
+    var output = str.replace('-', '+').replace('_', '/');
 
-    UrlUtilities.prototype.patternWithQueryString = function (url) {
-        return this.url(url) + ':?queryString:';
-    };
-
-    UrlUtilities.prototype.getReturnUrl = function (_default) {
-        var self = this;
-        var result = self.url(_default);
-
-        var context = router.viewModel();
-
-        if (context && context.route) {
-            var query = new _kocoQuery2.default(context.route.url);
-
-            if (query.params.returnTo) {
-                result = query.params.returnTo;
-            }
-        }
-
-        return result || '';
-    };
-
-    UrlUtilities.prototype.joinUrlParts = function () {
-        var result = '';
-        var urlParts = Array.prototype.slice.call(arguments, 0);
-
-        if (_kocoArrayUtilities2.default.isNotEmptyArray(urlParts)) {
-            for (var i = 0; i < urlParts.length; i++) {
-                var urlPart = urlParts[i];
-                var isLast = i == urlParts.length - 1;
-
-                if (urlPart && !isLast) {
-                    urlPart = _kocoStringUtilities2.default.trimRight(urlPart, '/');
-                    urlPart += '/';
-                }
-
-                result += urlPart;
-            }
-        }
-
-        return result;
-    };
-
-    function cleanUrl(url) {
-        var result = url || '';
-        result = result.replace(/#.*$/, '').replace(/^\/|\/$/g, '').toLowerCase();
-
-        return result;
+    switch (output.length % 4) {
+      case 0:
+        break;
+      case 2:
+        output += '==';
+        break;
+      case 3:
+        output += '=';
+        break;
+      default:
+        throw 'Illegal base64url string!';
     }
 
-    exports.default = new UrlUtilities();
+    return window.atob(output); //TODO: polifyll https://github.com/davidchambers/Base64.js
+  };
+
+  UrlUtilities.prototype.isSameUrl = function (url1, url2) {
+    return cleanUrl(url1) === cleanUrl(url2);
+  };
+
+  UrlUtilities.prototype.url = function (url, baseUrl) {
+    baseUrl = (baseUrl || _kocoConfigs2.default.baseUrl).replace(/^\/|\/$/g, '');
+    url = url.replace(/^\//g, '');
+
+    if (baseUrl) {
+      return '/' + baseUrl + '/' + url;
+    }
+
+    return '/' + url;
+  };
+
+  UrlUtilities.prototype.patternWithQueryString = function (url) {
+    return this.url(url) + ':?queryString:';
+  };
+
+  UrlUtilities.prototype.getReturnUrl = function (_default) {
+    var self = this;
+    var result = self.url(_default);
+
+    var context = _koco2.default.router.viewModel();
+
+    if (context && context.route) {
+      var query = new _kocoQuery2.default(context.route.url);
+
+      if (query.params.returnTo) {
+        result = query.params.returnTo;
+      }
+    }
+
+    return result || '';
+  };
+
+  UrlUtilities.prototype.joinUrlParts = function () {
+    var result = '';
+    var urlParts = Array.prototype.slice.call(arguments, 0);
+
+    if (_kocoArrayUtilities2.default.isNotEmptyArray(urlParts)) {
+      for (var i = 0; i < urlParts.length; i++) {
+        var urlPart = urlParts[i];
+        var isLast = i == urlParts.length - 1;
+
+        if (urlPart && !isLast) {
+          urlPart = _kocoStringUtilities2.default.trimRight(urlPart, '/');
+          urlPart += '/';
+        }
+
+        result += urlPart;
+      }
+    }
+
+    return result;
+  };
+
+  UrlUtilities.prototype.appendParams = function (url, data, traditionalParam) {
+    if (!_jquery2.default.isEmptyObject(data)) {
+      return '' + url + (url.indexOf('?') >= 0 ? '&' : '?') + _jquery2.default.param(data, !!traditionalParam);
+    }
+
+    return url;
+  };
+
+  function cleanUrl(url) {
+    var result = url || '';
+    result = result.replace(/#.*$/, '').replace(/^\/|\/$/g, '').toLowerCase();
+
+    return result;
+  }
+
+  exports.default = new UrlUtilities();
 });
--- src/url-utilities.js
+++ src/url-utilities.js
@@ -5,91 +5,100 @@ import configs from 'koco-configs';
 import arrayUtilities from 'koco-array-utilities';
 import stringUtitilies from 'koco-string-utilities';
 import Query from 'koco-query';
-
+import $ from 'jquery';
+import koco from 'koco';
 
 var UrlUtilities = function UrlUtilities() {};
 
 UrlUtilities.prototype.urlBase64Decode = function(str) {
-    var output = str.replace('-', '+').replace('_', '/');
+  var output = str.replace('-', '+').replace('_', '/');
 
-    switch (output.length % 4) {
-        case 0:
-            break;
-        case 2:
-            output += '==';
-            break;
-        case 3:
-            output += '=';
-            break;
-        default:
-            throw 'Illegal base64url string!';
-    }
+  switch (output.length % 4) {
+    case 0:
+      break;
+    case 2:
+      output += '==';
+      break;
+    case 3:
+      output += '=';
+      break;
+    default:
+      throw 'Illegal base64url string!';
+  }
 
-    return window.atob(output); //TODO: polifyll https://github.com/davidchambers/Base64.js
+  return window.atob(output); //TODO: polifyll https://github.com/davidchambers/Base64.js
 };
 
 UrlUtilities.prototype.isSameUrl = function(url1, url2) {
-    return cleanUrl(url1) === cleanUrl(url2);
+  return cleanUrl(url1) === cleanUrl(url2);
 };
 
 UrlUtilities.prototype.url = function(url, baseUrl) {
-    baseUrl = (baseUrl || configs.baseUrl).replace(/^\/|\/$/g, '');
-    url = url.replace(/^\//g, '');
+  baseUrl = (baseUrl || configs.baseUrl).replace(/^\/|\/$/g, '');
+  url = url.replace(/^\//g, '');
 
-    if (baseUrl) {
-        return '/' + baseUrl + '/' + url;
-    }
+  if (baseUrl) {
+    return '/' + baseUrl + '/' + url;
+  }
 
-    return '/' + url;
+  return '/' + url;
 };
 
 UrlUtilities.prototype.patternWithQueryString = function(url) {
-    return this.url(url) + ':?queryString:';
+  return this.url(url) + ':?queryString:';
 };
 
 UrlUtilities.prototype.getReturnUrl = function(_default) {
-    var self = this;
-    var result = self.url(_default);
+  var self = this;
+  var result = self.url(_default);
 
-    var context = router.viewModel();
+  var context = koco.router.viewModel();
 
-    if (context && context.route) {
-        var query = new Query(context.route.url);
+  if (context && context.route) {
+    var query = new Query(context.route.url);
 
-        if (query.params.returnTo) {
-            result = query.params.returnTo;
-        }
+    if (query.params.returnTo) {
+      result = query.params.returnTo;
     }
+  }
 
-    return result || '';
+  return result || '';
 };
 
 UrlUtilities.prototype.joinUrlParts = function() {
-    var result = '';
-    var urlParts = Array.prototype.slice.call(arguments, 0);
+  var result = '';
+  var urlParts = Array.prototype.slice.call(arguments, 0);
 
-    if (arrayUtilities.isNotEmptyArray(urlParts)) {
-        for (var i = 0; i < urlParts.length; i++) {
-            var urlPart = urlParts[i];
-            var isLast = i == (urlParts.length - 1);
+  if (arrayUtilities.isNotEmptyArray(urlParts)) {
+    for (var i = 0; i < urlParts.length; i++) {
+      var urlPart = urlParts[i];
+      var isLast = i == (urlParts.length - 1);
 
-            if (urlPart && !isLast) {
-                urlPart = stringUtitilies.trimRight(urlPart, '/');
-                urlPart += '/';
-            }
+      if (urlPart && !isLast) {
+        urlPart = stringUtitilies.trimRight(urlPart, '/');
+        urlPart += '/';
+      }
 
-            result += urlPart;
-        }
+      result += urlPart;
     }
+  }
 
-    return result;
+  return result;
+};
+
+UrlUtilities.prototype.appendParams = function(url, data, traditionalParam) {
+  if (!$.isEmptyObject(data)) {
+    return `${url}${(url.indexOf('?') >= 0 ? '&' : '?')}${$.param(data, !!traditionalParam)}`;
+  }
+
+  return url;
 };
 
 function cleanUrl(url) {
-    var result = url || '';
-    result = result.replace(/#.*$/, '').replace(/^\/|\/$/g, '').toLowerCase();
+  var result = url || '';
+  result = result.replace(/#.*$/, '').replace(/^\/|\/$/g, '').toLowerCase();
 
-    return result;
+  return result;
 }
 
 export default new UrlUtilities();
